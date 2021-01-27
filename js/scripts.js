704f2278f31a322b74b8c42363cc916360ceb3d5
Saoud <saoud@saoud.dev>
2021-01-26 20:50:59 -0800

    updates to js and html
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const food = $("input:radio[name=food]:checked").val();
+    const favoriteFood = $("input:radio[name=food]:checked").val();
+    console.log("Script Running!")
+    $(".food").append(favoriteFood);
+
+    $("#output").show();
+
     event.preventDefault();
   });
 });
